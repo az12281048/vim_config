44b40e650b358814b08a84c6b3271003cab30620
dengqinghua <dengqinghua@rd.tuan800.com>
2014-08-01 12:13:41 +0800

    添加vim配置

---
+++ monaco_font.sh
@@ -0,0 +1,6 @@
+ install monaco font
+cd ~/tmp
+wget http://www.gringod.com/wp-upload/MONACO.TTF
+sudo cp MONACO.TTF /usr/share/fonts/truetype/
+git clone https://github.com/pdf/ubuntu-mono-powerline-ttf.git ~/.fonts/ubuntu-mono-powerline-ttf
+sudo fc-cache -f -v
